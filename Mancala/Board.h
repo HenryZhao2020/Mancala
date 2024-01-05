653dbc4c473e586b46265a3c17b9beeef18e17a1
Henry Zhao <75873192+HenryZhao2020@users.noreply.github.com>
2024-01-05 12:13:23 -0800

    Added new files

---
+++ Mancala/Board.h
@@ -0,0 +1,120 @@
+#pragma once
+
+class Game;
+class GameBar;
+class Pocket;
+
+/**
+ * @brief Displays a Mancala board with stores on both sides
+ * and pockets on the top and bottom.
+ */
+class Board : public QFrame {
+    Q_OBJECT
+
+public:
+    /**
+     * @brief Initializes a new 'Board' instance.
+     * @param game The parent 'Game' instance.
+     */
+    Board(Game *game);
+    ~Board();
+
+    /**
+     * @brief Deposits red stones from the specified starting index.
+     * @param i The starting index.
+     */
+    void moveRed(int i);
+
+    /**
+     * @brief Deposits blue stones from the specified starting index.
+     * @param i The starting index.
+     */
+    void moveBlue(int i = -1);
+
+    /**
+     * @brief Displays the updated number of stones in each pocket and store.
+     */
+    void updateStones();
+
+    /**
+     * @brief Configures the board to enable or disable AI opponents.
+     * @param playAI Whether to play against AI.
+     */
+    void setPlayAI(bool playAI);
+
+    /**
+     * @brief Sets whether the board is interactive
+     * with keyboard and mouse.
+     * @param interactive Whether the board is interactive
+     * with keyboard and mouse.
+     */
+    void setInteractive(bool interactive);
+
+    /**
+     * @brief Returns whether the board is interactive
+     * with keyboard and mouse.
+     * @return Whether the board is interactive
+     * with keyboard and mouse.
+     */
+    bool isInteractive();
+
+private:
+    Game *game;
+    GameBar *gameBar;
+    // Store all pockets
+    Pocket *pockets[14];
+    // Whether the board is interactive with keyboard and mouse
+    bool interactive = true;
+
+    /**
+     * @brief Returns blue's next move calculated by AI.
+     * @return The index location of the next blue pocket.
+     */
+    int getNextBlue();
+
+    /**
+     * @brief Simulates the stone distribution from a blue pocket.
+     * @param i The starting index location.
+     * @return The index location of the pocket the last stone will land in.
+     */
+    int getBlueEndIndex(int i);
+
+    /**
+     * @brief Checks if all red pockets are empty.
+     * @return 'true' if all red pockets are empty; 'false' otherwise.
+     */
+    bool isRedEnded();
+
+    /**
+     * @brief Checks if all blue pockets are empty.
+     * @return 'true' if all blue pockets are empty; 'false' otherwise.
+     */
+    bool isBlueEnded();
+
+    /**
+     * @brief Checks the winner after a round ends.
+     */
+    void checkWinner();
+
+    /**
+     * @brief Ends the game.
+     * @param icon The displayed icon on the game bar.
+     * @param text The displayed text on the game bar.
+     */
+    void end(const QIcon &icon, const QString &text);
+
+    /**
+     * @brief Handles the end of game if red wins.
+     */
+    void redWins();
+
+    /**
+     * @brief Handles the end of game if blue wins.
+     */
+    void blueWins();
+
+    /**
+     * @brief Handles the end of game if red and blue tie.
+     */
+    void tie();
+};
